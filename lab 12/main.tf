1e1eed81b611f4997861bf501c93cc5708f5f49d
Kunderenko <39592711+Kunderenko@users.noreply.github.com>
2022-06-23 14:38:23 +0300

    Update main.tf

--- lab 12/main.tf
+++ lab 12/main.tf
@@ -1,7 +1,7 @@
 data "aws_availability_zones" "available" {}
 
 resource "aws_vpc" "vpc" {
-  cidr_block           = "10.0.0.0/16"
+  cidr_block           = "10.0.10.0/16"
   enable_dns_hostnames = true
   enable_dns_support   = true
   tags = {
